c1fc887dda18e89f1871562e0e68931de1b34c92
Archisman-Mridha <archismanmridha12345@gmail.com>
2022-05-19 12:45:46 +0530

    adding webpack publicpath option for container

--- web/container/webpack.config.prod.js
+++ web/container/webpack.config.prod.js
@@ -3,6 +3,9 @@ const moduleFederationConfig = require("./module-federation.config")
 
 module.exports = withModuleFederation({
     ...moduleFederationConfig,
+    output: {
+        publicPath: "/container/"
+    },
 
     remotes: [["shared", "//localhost:4201/"]]
 })
--- web/shared/webpack.config.prod.js
+++ web/shared/webpack.config.prod.js
@@ -2,9 +2,5 @@ const withModuleFederation = require("@nrwl/react/module-federation")
 const moduleFederationConfig = require("./module-federation.config")
 
 module.exports= withModuleFederation({
-    ...moduleFederationConfig,
-
-    output: {
-        publicPath: "/container/"
-    }
+    ...moduleFederationConfig
 })
