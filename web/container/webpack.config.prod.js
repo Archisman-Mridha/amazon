db0f5a7c46bbd2a77c43b2262b525d5926c3839b
Archisman-Mridha <archismanmridha12345@gmail.com>
2022-05-19 15:13:31 +0530

    test commit

--- web/container/webpack.config.prod.js
+++ web/container/webpack.config.prod.js
@@ -3,9 +3,6 @@ const moduleFederationConfig = require("./module-federation.config")
 
 module.exports = withModuleFederation({
     ...moduleFederationConfig,
-    output: {
-        publicPath: "/container/"
-    },
 
     // remotes: [["shared", "//localhost:4201/"]]
 })
